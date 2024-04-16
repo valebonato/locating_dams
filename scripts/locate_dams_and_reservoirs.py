2d4e521d3534c43e250b9fa98c6dbe9867846a49
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-04-16 13:37:56 +0200

    test to solve more than one pixel problem - based on the shortest distance to AHA

--- scripts/locate_dams_and_reservoirs.py
+++ scripts/locate_dams_and_reservoirs.py
@@ -11,6 +11,10 @@ import virtualOS as vos
 # table/column input file - this should be sorted based on reservoir capacities (the largest the lower ids)
 column_input_file = "../aha_hydropowers/existing_reservoirs_v2024-04-10_selected_no-headers.csv"
 
+# - for testing with 74 and 85 (multiple pixel problem)
+column_input_file = "../aha_hydropowers/existing_reservoirs_v2024-04-10_selected_no-headers-test74and85.csv"
+
+
 # set the clone (your map grid system and extent)
 clone_map_file = "../pcrglobwb_maps/lddsound_05min_version_20210330.map"
 pcr.setclone(clone_map_file)
@@ -60,8 +64,9 @@ rel_dif_catchment_area = pcr.abs(aha_catchment_area_km2 - dam_ids_pcrglobwb_catc
 threshold = 0.1
 number_of_dams = 131
 
-# ~ # for testing
-# ~ number_of_dams = 10
+# for testing
+number_of_dams = 10
+number_of_dams = 2
 
 
 def get_distances_to_a_reference_latlon_coodinate_in_meter(lon_pcrmap, lat_pcrmap, lon_ref, lat_ref):
